550b0cd70df0eb198d8ce86bbbaf26caff835306
Cmb <71194682+29cmb@users.noreply.github.com>
2023-12-24 11:31:23 -0500

    Barebones for signup

--- index.js
+++ index.js
@@ -2,7 +2,18 @@ const express = require('express');
 const path = require('path')
 const app = express();
 
-app.use(express.static(path.join(__dirname, 'views')));
-app.listen(3000, () => {
-  console.log('server started');
+require("dotenv").config()
+
+app.use(express.static(path.join(__dirname, process.env.DIR)));
+app.listen(process.env.PORT, () => {
+  console.log(`\n   _____           _    __      _ _                 
+  |  __ \\         | |  / _|    | (_)                
+  | |__) |__  _ __| |_| |_ ___ | |_  ___  _   _ ___ 
+  |  ___/ _ \\| '__| __|  _/ _ \\| | |/ _ \\| | | / __|
+  | |  | (_) | |  | |_| || (_) | | | (_) | |_| \\__ \\
+  |_|   \\___/|_|   \\__|_| \\___/|_|_|\\___/ \\__,_|___/
+                                                    
+  Created by DevCmb
+  Views Directory: ${process.env.DIR}                                                  
+  \nLaunched on localhost:${process.env.PORT}`);
 });
